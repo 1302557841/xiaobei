bfb0b51b0881ae3c15aefd992bb58d8966c5876d
1302557841 <39792363+1302557841@users.noreply.github.com>
2022-10-20 20:15:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def ts_send():
         #response = requests.post(url=url,headers=headers,params=payload)
         #推送
         message = news_60s()
-        url = 'https://sc.ftqq.com/SCT27293TKfePhl6wGNlIzy0FZlSGOIW2.send'
+        url = 'https://sc.ftqq.com/SCT177254TFUHwzdngUHGmQ7z594NDsEFi.send'
         desp = message
         data = {
         'text': {stoday} +'· 60秒新闻',
@@ -361,7 +361,7 @@ if __name__ == '__main__':
         if status == 200:
             print("恭喜您打卡成功啦！")
             message = yiyan()+news_60s()
-            url = 'https://sc.ftqq.com/SCT27293TKfePhl6wGNlIzy0FZlSGOIW2.send'
+            url = 'https://sc.ftqq.com/SCT177254TFUHwzdngUHGmQ7z594NDsEFi.send'
             desp = message
             data = {
             'text': '小北自动打卡成功啦!',
@@ -381,7 +381,7 @@ if __name__ == '__main__':
         else:
             print("Error：" + json.loads(respond)['msg'])
             message =  "Error原因：" + json.loads(respond)['msg'] + "\n\n" +yiyan()+news_60s()
-            url = 'https://sc.ftqq.com/SCT27293TKfePhl6wGNlIzy0FZlSGOIW2.send'
+            url = 'https://sc.ftqq.com/SCT177254TFUHwzdngUHGmQ7z594NDsEFi.send'
             desp = message
             data = {
             'text': '小北自动打卡失败!',
